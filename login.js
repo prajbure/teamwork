4fb713b8bbe18037624d108212fb4456aa838bd4
prajbure <prajwalambure7@gmail.com>
2026-03-05 16:50:12 +0530

    Enhance login function with remember me feature
    Refactored login function to include remember me functionality and improved user feedback.

--- login.js
+++ login.js
@@ -2,25 +2,45 @@ const users={
 "Prajwal":"1234",
 "Parikshith":"1234",
 "Manjunath":"1234",
-"Member4":"1234",
-"Member5":"1234",
-"Member6":"1234"
+"Chandana Hublikar":"1234",
+"Hemalatha":"1234",
+"Chandana G":"1234"
+}
+
+window.onload=function(){
+
+let savedUser=localStorage.getItem("rememberUser")
+
+if(savedUser){
+
+document.getElementById("username").value=savedUser
+document.getElementById("remember").checked=true
+
+}
+
 }
 
 function login(){
 
-let u=document.getElementById("username").value.trim()
-let p=document.getElementById("password").value.trim()
+let username=document.getElementById("username").value.trim()
+let password=document.getElementById("password").value.trim()
+let remember=document.getElementById("remember").checked
 
-if(users[u]===p){
+if(users[username]===password){
 
-localStorage.setItem("user",u)
+localStorage.setItem("user",username)
+
+if(remember){
+localStorage.setItem("rememberUser",username)
+}else{
+localStorage.removeItem("rememberUser")
+}
+
 window.location="dashboard.html"
 
-}
-else{
+}else{
 
-alert("Invalid Login")
+alert("Invalid Username or Password")
 
 }
 
